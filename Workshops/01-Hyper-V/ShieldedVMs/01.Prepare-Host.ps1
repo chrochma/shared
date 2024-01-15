e031399514799754cf818c901642c4a2e2726fb3
chrochma <46521602+chrochma@users.noreply.github.com>
2024-01-15 09:43:22 +0100

    v1.0.0

--- Workshops/01-Hyper-V/ShieldedVMs/01.Prepare-Host.ps1
+++ Workshops/01-Hyper-V/ShieldedVMs/01.Prepare-Host.ps1
@@ -1,9 +1,6 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Confirm:$false
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name DoNotOpenAtLogon -Value 1        
-    $Date = Get-Date -Format "dd.MM.yyyy hh:mm:ss"
-    "Starting Initial Configuration of $Computername, $Date" | Out-File -FilePath $Loglocation\InitialConfiguration.log -Append
-
     $Result=Install-WindowsFeature -Name "Hyper-V" -ErrorAction SilentlyContinue
     if ($result.ExitCode -eq "failed"){
         Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online -NoRestart 
@@ -15,12 +12,16 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name DoNotOpenA
     
     Set-TimeZone -Id "W. Europe Standard Time"
     Write-Host "Timezone set to W. Europe Standard Time" -ForegroundColor Cyan
-    
-
+  
     get-disk | Where-Object PartitionStyle -like RAW | Initialize-Disk -PartitionStyle GPT | New-Partition -UseMaximumSize -AssignDriveLetter
 
     $disks = get-disk | Where-Object Size -gt 1000000000000 
     foreach($Disk in $Disks){
         $Disk | New-Partition -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel "DATA" -Confirm:$false 
     }
-    
+  
+    New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force
+
+    Write-Host "Pre Installation succeeded, press any key to restart." -ForegroundColor Cyan
+    Pause
+    Restart-Computer -Force -Confirm:$false
